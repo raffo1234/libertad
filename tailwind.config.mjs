bbda5b373c61f9910ad45f3067826b62d315537e
raffo1234 <rhmcord@gmail.com>
2026-02-26 11:47:48 -0500

    feat: add typology section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,25 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      mulish: ["Mulish", "sans-serif"],
-      manrope: ["Manrope", "sans-serif"],
-      urbanist: ["Urbanist", "sans-serif"],
-      tanPearl: ["Tan Pearl", "sans-serif"],
-    },
     extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
+        tanPearl: ["Tan Pearl", "sans-serif"],
+      },
+      colors: {
+        "custom-orange": "#ff9100",
+      },
       keyframes: {
         scale: {
-          "0%": { transform: "scale(0.90)", opacity: 0 },
-          "100%": { transform: "scale(1)", opacity: 1 },
+          "0%": { transform: "scale(0.90)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
         },
       },
       animation: {
-        scale: "scale 0.4s",
+        scale: "scale 0.4s ease-out",
       },
     },
   },
