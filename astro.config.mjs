31d28cd025d418fdfd9468d233f2274fdbfa2991
raffo1234 <rhmcord@gmail.com>
2026-02-27 15:45:23 -0500

    feat: add sitemap and favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-
+import sitemap from "@astrojs/sitemap";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
+  integrations: [tailwind(), icon(), react(), sitemap()],
   site: "https://www.galvez1519.com",
 });
